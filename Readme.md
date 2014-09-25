bc1da2410c010c4fced276f9e65c4e5bf523c6d7
Alexandre Rio <contact@alexrio.fr>
2014-09-25 21:40:03 +0200

    initial commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# stat_logger
+service script to put into /etc/init.d/
+C code that daemonize a call to the bash script every 5min
---
+++ stat.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+JQ="jq"
+BTC_ADDRESS=""
+LOG="log.csv"
+DATA="data.json"
+
+
+TIME=`date +"%d/%m/%y %T"`
+wget http://wafflepool.com/tmp_api?address=$BTC_ADDRESS -O $DATA
+
+TOTAL_HASH_RATE=`cat $DATA | $JQ '.hash_rate'`
+WORKER1_HASH_RATE=`cat $DATA | $JQ ''.worker_hashrates[1].hashrate''`
+WORKER2_HASH_RATE=`cat $DATA | $JQ ''.worker_hashrates[2].hashrate''`
+
+CONFIRMED=`cat $DATA | $JQ '.balances.confirmed'`
+SENT=`cat $DATA | $JQ '.balances.sent'`
+UNCONVERTED=`cat $DATA | $JQ '.balances.unconverted'`
+
+echo $TIME,$TOTAL_HASH_RATE,$WORKER1_HASH_RATE,$WORKER2_HASH_RATE,$CONFIRMED,$SENT,$UNCONVERTED >> $LOG
+rm $DATA
